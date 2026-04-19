98505e0339cf697bff2f9374871e0a4cbd1edce5
satarnmakcom <satarnmakcom@gmail.com>
2026-04-19 10:28:11 +0700

    Create 0007.c

---
+++ 0000-0043/0007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int R;
+double A,B;
+double PI=3.141592653589793;
+scanf("%d",&R);
+A = R*R*PI;
+B = 2*R*R;
+printf("%.6f\n",A);
+printf("%.6f",B);
+ return 0;
+}
